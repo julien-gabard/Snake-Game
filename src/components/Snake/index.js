7968c9bbdeae1d374cf55bb1134286f968b71f62
julien-gabard <juliengabard@hotmail.fr>
2020-07-18 16:58:14 +0200

    editing component snake

---
+++ src/components/Snake/index.js
@@ -0,0 +1,15 @@
+// == Import npm
+import React from 'react';
+
+// == Import SCSS
+import './snake.scss';
+
+// == Component
+const Snake = () => (
+  <div className="snake">
+    Snake
+  </div>
+);
+
+// == Export
+export default Snake;
