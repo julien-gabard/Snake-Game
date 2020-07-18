15106a57f166c0665c7f0a0aee83e951a8503993
julien-gabard <juliengabard@hotmail.fr>
2020-07-18 16:58:44 +0200

    editing component food

---
+++ src/components/Food/index.js
@@ -0,0 +1,15 @@
+// == Import npm
+import React from 'react';
+
+// == Import SCSS
+import './food.scss';
+
+// == Component
+const Food = () => (
+  <div className="snake">
+    Food
+  </div>
+);
+
+// == Export
+export default Food;
