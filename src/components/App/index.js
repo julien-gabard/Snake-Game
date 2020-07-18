936d3d1c8114df41608cb3d8918712ad95d91839
julien-gabard <juliengabard@hotmail.fr>
2020-07-18 17:13:48 +0200

    add randomCoordinate and initialState

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,15 +1,47 @@
 // == Import npm
 import React from 'react';
 
+// == Import Components
+import Snake from 'src/components/Snake';
+
 // == Import
 import './app.scss';
 
 // == Composant
-const App = () => (
-  <div className="app">
-    <h1>Composant : App</h1>
-  </div>
-);
+/**
+ * Allows you to generate a random number
+ */
+const getRandomCoordinates = () => {
+  const min = 1;
+  const max = 98;
+  const x = Math.floor((Math.random() * (max - min + 1) + min) / 2) * 2;
+  const y = Math.floor((Math.random() * (max - min + 1) + min) / 2) * 2;
+  return [x, y];
+};
+
+const initialState = {
+  food: getRandomCoordinates(),
+  speed: 200,
+  direction: 'RIGHT',
+  snakeDots: [
+    [0, 0],
+    [2, 0],
+  ],
+};
+
+class App extends React.Component {
+  state = initialState;
+
+  render() {
+    const { snakeDots } = this.state;
+
+    return (
+      <div className="app">
+        <Snake snakeDots={snakeDots} />
+      </div>
+    );
+  }
+}
 
 // == Export
 export default App;
--- src/components/Snake/index.js
+++ src/components/Snake/index.js
@@ -1,15 +1,29 @@
 // == Import npm
 import React from 'react';
+import PropTypes from 'prop-types';
 
 // == Import SCSS
 import './snake.scss';
 
 // == Component
-const Snake = () => (
-  <div className="snake">
-    Snake
+const Snake = ({ snakeDots }) => (
+  <div>
+    {snakeDots.map((dot, i) => {
+      const style = {
+        left: `${dot[0]}%`,
+        top: `${dot[1]}%`,
+      };
+      return (
+        <div className="snake-dot" key={i} style={style}></div>
+      );
+    })}
   </div>
 );
+
+// == PropTypes
+Snake.propTypes = {
+  snakeDots: PropTypes.array.isRequired,
+};
 
 // == Export
 export default Snake;
