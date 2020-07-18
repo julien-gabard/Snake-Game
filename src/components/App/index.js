2108854083f83f8b8da4148971ece5cac7fc4be1
julien-gabard <juliengabard@hotmail.fr>
2020-07-18 17:41:59 +0200

    add moveSnake, onKeyDown, componentDidMount

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -33,6 +33,77 @@ const initialState = {
 class App extends React.Component {
   state = initialState;
 
+  componentDidMount() {
+    const { speed } = this.state;
+    setInterval(this.moveSnake, speed);
+    document.onkeydown = this.onKeyDown;
+  }
+
+  /**
+   * binding of arrow keys to state "direction"
+   * @param evt "event"
+   */
+  onKeyDown = (evt) => {
+    evt = evt || window.event;
+    switch (evt.keyCode) {
+      case 38:
+        this.setState({
+          direction: 'UP',
+        });
+        break;
+      case 40:
+        this.setState({
+          direction: 'DOWN',
+        });
+        break;
+      case 37:
+        this.setState({
+          direction: 'LEFT',
+        });
+        break;
+      case 39:
+        this.setState({
+          direction: 'RIGHT',
+        });
+        break;
+      default:
+    }
+  }
+
+  /**
+   * snake movement in one direction
+   */
+  moveSnake = () => {
+    const { snakeDots, direction } = this.state;
+
+    const dots = [...snakeDots];
+    let head = dots[dots.length - 1];
+
+    switch (direction) {
+      case 'RIGHT':
+        head = [head[0] + 2, head[1]];
+        break;
+      case 'LEFT':
+        head = [head[0] - 2, head[1]];
+        break;
+      case 'DOWN':
+        head = [head[0], head[1] + 2];
+        break;
+      case 'UP':
+        head = [head[0], head[1] - 2];
+        break;
+      default:
+    }
+    // I add the head element
+    dots.push(head);
+    // I remove the first element
+    dots.shift();
+    this.setState({
+      // I add the new coordinates in state
+      snakeDots: dots,
+    });
+  }
+
   render() {
     const { snakeDots, food } = this.state;
 
